28da6b9c4790cfc90aaac920e2baae003bdbc7e3
Andy <andreii.kachanov@aily.team>
2024-04-02 00:57:23 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,424 @@
+<!DOCTYPE html>
+<html>
+<head> 
+	<title>Box&amp;Cube</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+	<div class="wrapper vcard">
+		<header>
+			<div class="light_left_part">
+				<a class="logo" href="index.php">
+					<h1 class="fn org">Box &amp; Cube</h1>
+					<h2 class="creative">create &amp; innovate solutions</h2>
+				</a>
+				<ul class="top_menu">
+					<li><a href="#">Home</a></li>
+					<li><a href="#">About</a></li>
+					<li><a href="#">Contact</a></li>
+					<li><a href="#">Blog</a></li>
+				</ul>
+			</div>
+			<div class="light_right_part"></div>
+			<div class="light_center_part"></div>
+			<div class="dark_top"></div>			
+			<div class="dark_center">
+				<ul class="subscribe_menu">
+					<li><img src="img/subscr_menu.png" alt=""></li>
+					<li>Please subscribe to:</li>
+					<li><a href="">Posts</a></li>
+					<li><a href="">Comments</a></li>
+					<li><a href="">Email</a></li>	
+				</ul>
+				<form class="search_form clear" name="searchform" action="#" method="post">
+						<input type="text" name="search" class="text" placeholder="Enter Search Keywords" required>
+						<input type="submit" value="" class="submit">
+				</form>
+			</div>
+			<div class="dark_bottom"></div>
+		</header>
+		<nav class="main_menu">
+			<ul>
+				<li><a href="#">WEB PROFESSIONALS</a></li>
+				<li><a href="#">BUSINESS</a></li>
+				<li><a href="#">WEB DSIGN</a></li>
+				<li><a href="#">MEDIA</a></li>
+				<li><a href="#">ENTERTAINTMENT</a></li>
+				<li><a href="#">NON-PROFIT</a></li>
+				<li><a href="#">INSTITUTIONS</a></li>
+				<li><a href="#">PORTALS</a></li>
+			</ul>
+		</nav>
+		<main class="content_section">
+		<div class="layer_top"></div>
+		<div class="layer_bottom"></div>
+			<ul class="items">
+				<li>
+					<div class="item_wrap">
+						<div class="item_header">
+							<a class="item_name" href="">Let’s Make Movies</a>
+							<img src="img/comments.png" alt="">
+							<img src="img/comments.png" alt="">
+							<span class="count_comments">10</span>
+						</div>
+						<a class="item_img" href="#">
+							<img src="img/items/item1.png" alt="">
+						</a>
+						<div class="item_rating">
+							<span class="stars">
+								<img src="img/star_gold.png" alt="">
+								<img src="img/star_gold.png" alt="">
+								<img src="img/star_gold.png" alt="">
+								<img src="img/star_gold.png" alt="">
+								<img src="img/star_gray.png" alt="">
+							</span>
+							<a href="#" class="write_comment">Write a Comment</a>
+						</div>
+					</div>
+				</li>
+								<li>
+					<div class="item_wrap">
+						<div class="item_header">
+							<a class="item_name" href="">Paint Me Now</a>
+							<img src="img/comments.png" alt="">
+							<span class="count_comments">14</span>
+						</div>
+						<a class="item_img" href="#">
+							<img src="img/items/item2.png" alt="">
+						</a>
+						<div class="item_rating">
+							<span class="stars">
+								<img src="img/star_gold.png" alt="">
+								<img src="img/star_gold.png" alt="">
+								<img src="img/star_gold.png" alt="">
+								<img src="img/star_gold.png" alt="">
+								<img src="img/star_gray.png" alt="">
+							</span>
+							<a href="#" class="write_comment">Write a Comment</a>
+						</div>
+					</div>
+				</li>
+								<li>
+					<div class="item_wrap">
+						<div class="item_header">
+							<a class="item_name" href="">System Gears</a>
+							<img src="img/comments.png" alt="">
+							<span class="count_comments">19</span>
+						</div>
+						<a class="item_img" href="#">
+							<img src="img/items/item3.png" alt="">
+						</a>
+						<div class="item_rating">
+							<span class="stars">
+								<img src="img/star_gold.png" alt="">
+								<img src="img/star_gold.png" alt="">
+								<img src="img/star_gold.png" alt="">
+								<img src="img/star_gold.png" alt="">
+								<img src="img/star_gray.png" alt="">
+							</span>
+							<a href="#" class="write_comment">Write a Comment</a>
+						</div>
+					</div>
+				</li>
+								<li>
+					<div class="item_wrap">
+						<div class="item_header">
+							<a class="item_name" href="">iPhone Icons</a>
+							<img src="img/comments.png" alt="">
+							<span class="count_comments">76</span>
+						</div>
+						<a class="item_img" href="#">
+							<img src="img/items/item4.png" alt="">
+						</a>
+						<div class="item_rating">
+							<span class="stars">
+								<img src="img/star_gold.png" alt="">
+								<img src="img/star_gold.png" alt="">
+								<img src="img/star_gold.png" alt="">
+								<img src="img/star_gold.png" alt="">
+								<img src="img/star_gray.png" alt="">
+							</span>
+							<a href="#" class="write_comment">Write a Comment</a>
+						</div>
+					</div>
+				</li>
+								<li>
+					<div class="item_wrap">
+						<div class="item_header">
+							<a class="item_name" href="">Contact Me Icons</a>
+							<img src="img/comments.png" alt="">
+							<span class="count_comments">11</span>
+						</div>
+						<a class="item_img" href="#">
+							<img src="img/items/item5.png" alt="">
+						</a>
+						<div class="item_rating">
+							<span class="stars">
+								<img src="img/star_gold.png" alt="">
+								<img src="img/star_gold.png" alt="">
+								<img src="img/star_gold.png" alt="">
+								<img src="img/star_gold.png" alt="">
+								<img src="img/star_gray.png" alt="">
+							</span>
+							<a href="#" class="write_comment">Write a Comment</a>
+						</div>
+					</div>
+				</li>
+								<li>
+					<div class="item_wrap">
+						<div class="item_header">
+							<a class="item_name" href="">Hardware Support</a>
+							<img src="img/comments.png" alt="">
+							<span class="count_comments">2</span>
+						</div>
+						<a class="item_img" href="#">
+							<img src="img/items/item6.png" alt="">
+						</a>
+						<div class="item_rating">
+							<span class="stars">
+								<img src="img/star_gold.png" alt="">
+								<img src="img/star_gold.png" alt="">
+								<img src="img/star_gold.png" alt="">
+								<img src="img/star_gold.png" alt="">
+								<img src="img/star_gray.png" alt="">
+							</span>
+							<a href="#" class="write_comment">Write a Comment</a>
+						</div>
+					</div>
+				</li>
+								<li>
+					<div class="item_wrap">
+						<div class="item_header">
+							<a class="item_name" href="">Website Review</a>
+							<img src="img/comments.png" alt="">
+							<span class="count_comments">10</span>
+						</div>
+						<a class="item_img" href="#">
+							<img src="img/items/item7.png" alt="">
+						</a>
+						<div class="item_rating">
+							<span class="stars">
+								<img src="img/star_gold.png" alt="">
+								<img src="img/star_gold.png" alt="">
+								<img src="img/star_gold.png" alt="">
+								<img src="img/star_gold.png" alt="">
+								<img src="img/star_gray.png" alt="">
+							</span>
+							<a href="#" class="write_comment">Write a Comment</a>
+						</div>
+					</div>
+				</li>
+								<li>
+					<div class="item_wrap">
+						<div class="item_header">
+							<a class="item_name" href="">Let’s Make Movies</a>
+							<img src="img/comments.png" alt="">
+							<span class="count_comments">13</span>
+						</div>
+						<a class="item_img" href="#">
+							<img src="img/items/item8.png" alt="">
+						</a>
+						<div class="item_rating">
+							<span class="stars">
+								<img src="img/star_gold.png" alt="">
+								<img src="img/star_gold.png" alt="">
+								<img src="img/star_gold.png" alt="">
+								<img src="img/star_gold.png" alt="">
+								<img src="img/star_gray.png" alt="">
+							</span>
+							<a href="#" class="write_comment">Write a Comment</a>
+						</div>
+					</div>
+				</li>
+								<li>
+					<div class="item_wrap">
+						<div class="item_header">
+							<a class="item_name" href="">Blue Tubes</a>
+							<img src="img/comments.png" alt="">
+							<span class="count_comments">4</span>
+						</div>
+						<a class="item_img" href="#">
+							<img src="img/items/item9.png" alt="">
+						</a>
+						<div class="item_rating">
+							<span class="stars">
+								<img src="img/star_gold.png" alt="">
+								<img src="img/star_gold.png" alt="">
+								<img src="img/star_gold.png" alt="">
+								<img src="img/star_gold.png" alt="">
+								<img src="img/star_gray.png" alt="">
+							</span>
+							<a href="#" class="write_comment">Write a Comment</a>
+						</div>
+					</div>
+				</li>
+								<li>
+					<div class="item_wrap">
+						<div class="item_header">
+							<a class="item_name" href="">Throw Me Away</a>
+							<img src="img/comments.png" alt="">
+							<span class="count_comments">33</span>
+						</div>
+						<a class="item_img" href="#">
+							<img src="img/items/item10.png" alt="">
+						</a>
+						<div class="item_rating">
+							<span class="stars">
+								<img src="img/star_gold.png" alt="">
+								<img src="img/star_gold.png" alt="">
+								<img src="img/star_gold.png" alt="">
+								<img src="img/star_gold.png" alt="">
+								<img src="img/star_gray.png" alt="">
+							</span>
+							<a href="#" class="write_comment">Write a Comment</a>
+						</div>
+					</div>
+				</li>
+								<li>
+					<div class="item_wrap">
+						<div class="item_header">
+							<a class="item_name" href="">Got Goffee?</a>
+							<img src="img/comments.png" alt="">
+							<span class="count_comments">15</span>
+						</div>
+						<a class="item_img" href="#">
+							<img src="img/items/item11.png" alt="">
+						</a>
+						<div class="item_rating">
+							<span class="stars">
+								<img src="img/star_gold.png" alt="">
+								<img src="img/star_gold.png" alt="">
+								<img src="img/star_gold.png" alt="">
+								<img src="img/star_gold.png" alt="">
+								<img src="img/star_gray.png" alt="">
+							</span>
+							<a href="#" class="write_comment">Write a Comment</a>
+						</div>
+					</div>
+				</li>
+								<li>
+					<div class="item_wrap">
+						<div class="item_header">
+							<a class="item_name" href="">Housing For You!</a>
+							<img src="img/comments.png" alt="">
+							<span class="count_comments">13</span>
+						</div>
+						<a class="item_img" href="#">
+							<img src="img/items/item12.png" alt="">
+						</a>
+						<div class="item_rating">
+							<span class="stars">
+								<img src="img/star_gold.png" alt="">
+								<img src="img/star_gold.png" alt="">
+								<img src="img/star_gold.png" alt="">
+								<img src="img/star_gold.png" alt="">
+								<img src="img/star_gray.png" alt="">
+							</span>
+							<a href="#" class="write_comment">Write a Comment</a>
+						</div>
+					</div>
+				</li>
+			</ul>
+			<div class="clear"></div>
+			<div class="pagination">
+				<p>You are looking at <span>page 1 of 9</span></p>
+				<ul>
+					<li>
+						<a href="#" class="pages"><span>1</span></a>
+					</li>
+					<li>
+						<a href="#">2</a>
+					</li>
+					<li>
+						<a href="#">3</a>
+					</li>
+					<li>
+						<a href="#">4</a>
+					</li>
+					<li>
+						<a href="#">5</a>
+					</li>
+					<li>
+						<a href="#">5</a>
+					</li>
+					<li>
+						<a href="#">6</a>
+					</li>
+					<li>
+						<a href="#">7</a>
+					</li>
+					<li>
+						<a href="#">8</a>
+					</li>
+					<li>
+						<a href="#">9</a>
+					</li>
+				</ul>
+				<div class="clear"></div>
+			</div>	
+		</main>
+		<div class="bottom_menu">
+			<div class="about_us">
+				<h2>About Us</h2>
+				<ul>
+					<li><a href="#">Our Company</a></li>
+					<li><a href="#">Our Blog</a></li>
+					<li><a href="#">Submit a Site</a></li>
+					<li><a href="#">Contact Us</a></li>
+					<li><a href="#">Help &amp; Terms</a></li>
+					<li><a href="#">Read Our FAQ</a></li>
+				</ul>
+			</div>
+			<div class="popular">
+				<h2>Popular Topics</h2>
+				<ul>
+					<li><a href="#">Trends &amp; Technology</a><span>(30)</span></li>
+					<li><a href="#">Design Companies</a><span>(2699)</span></li>
+					<li><a href="#">Design Freelancers</a><span>(23)</span></li>
+					<li><a href="#">Web Portfolios</a><span>(16)</span></li>
+					<li><a href="#">Web Development</a><span>(23)</span></li>
+					<li><a href="#">General Icons</a><span>(34)</span></li>
+				</ul>
+			</div>
+			<div class="follow">
+				<h2>Follow Us Here</h2>
+				<div class="social">
+					<a href=""><img src="img/follow_us/1.png" alt=""></a>
+					<a href=""><img src="img/follow_us/2.png" alt=""></a>
+					<a href=""><img src="img/follow_us/3.png" alt=""></a>
+					<a href=""><img src="img/follow_us/4.png" alt=""></a>
+					<a href=""><img src="img/follow_us/5.png" alt=""></a>
+					<a href=""><img src="img/follow_us/6.png" alt=""></a>
+					<a href=""><img src="img/follow_us/7.png" alt=""></a>
+					<a href=""><img src="img/follow_us/8.png" alt=""></a>
+					<a href=""><img src="img/follow_us/9.png" alt=""></a>
+					<a href=""><img src="img/follow_us/10.png" alt=""></a>
+					<a href=""><img src="img/follow_us/11.png" alt=""></a>
+					<a href=""><img src="img/follow_us/12.png" alt=""></a>
+					<a href=""><img src="img/follow_us/13.png" alt=""></a>
+					<a href=""><img src="img/follow_us/14.png" alt=""></a>
+					<a href=""><img src="img/follow_us/15.png" alt=""></a>
+				</div>
+			</div>
+			<div class="twitter">
+				<h2>Twitter Updates</h2>
+				<div class="bg">
+					<a class="white_color" href="#">@bogothemes</a> <span>there is a new designer in town people!</span>
+					<p>Check it out: <a href="#">http://bit.urds</a></p>
+				</div>
+			</div>
+			<br class="clear">
+		</div>
+		<footer>
+			<p class="copyright">&copy; Copyright 2015. Box&amp;Cube Theme. Designed by <a href="http://google.com">Andrey Kachanov</a>.</p>
+			<ul class="top_menu">
+				<li><a href="#">Home</a></li>
+				<li><a href="#">About</a></li>
+				<li><a href="#">Contact</a></li>
+				<li><a href="#">Blog</a></li>
+			</ul>
+			<br class="clear">	
+		</footer>	
+	</div>
+</body>
